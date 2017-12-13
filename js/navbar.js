65a57d5792c8b71087b2f919b8c15f524d2fa108
Anunay Mahajan <anunaymahajan@hotmail.com>
2017-12-13 23:52:05 +0530

    revision

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-            $("button.navbar-toggle").click();
-        }
-    });
-});
-=======
 $(document).click(function(e) {
 	if (!$(e.target).is('a')) {
     	$('.collapse').collapse('hide');	    
     }
 });
->>>>>>> 7655d45cfe4ea479ff470c98c79edaa615219c1a
